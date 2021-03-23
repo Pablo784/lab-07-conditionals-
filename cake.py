fd28a25364dc049be487fc35f20274f6bfbce9f2
Pablo Vasquez <pablo_vasquez@Pablos-MacBook-Pro.local>
2021-03-23 17:48:02 -0400

    sending two html files and one python file

---
+++ cake.py
@@ -0,0 +1,8 @@
+cakeornot = input("Do you want cake? (yes/no)")
+
+if cakeornot == "yes" or cakeornot == "YES" or cakeornot == "y":
+    print("Have some cake!")
+elif cakeornot == "no" or cakeornot == "NO" or cakeornot or cakeornot == "n":
+    print("No cake for you!")
+else:
+    print("Sorry, I don't understand.")
