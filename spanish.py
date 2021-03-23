a78e561e8b5d60966bf6adeacba7ff4284f19e43
Pablo Vasquez <pablo_vasquez@Pablos-MacBook-Pro.local>
2021-03-23 18:30:41 -0400

    I give you the python version of the spanish.html file

---
+++ spanish.py
@@ -0,0 +1,9 @@
+entospan = input("Give me a word in English. It can be can be cat, dog, & horse.");
+if (entospan == "cat" or entospan == "CAT"):
+     print("gato")
+if (entospan == "dog"  or entospan == "DOG"):
+     print("perro")
+if(entospan == "horse" or entospan == "HORSE"):
+     print("caballo")
+else:
+    print("no entiendo")
